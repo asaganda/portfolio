e941dc729ac6b167832cf1b20c2232e8f353bf58
Andrew <Asaganda@gmail.com>
2022-11-02 16:50:54 -0400

    setup logic for showing 1 content when clicked in nav bar and hiding others

--- app.js
+++ app.js
@@ -1,3 +1,68 @@
+
 $(() => {
-    console.log('javascript jquery works');
+    // $('#nav-bio').on('click', () => {
+    //     $('#bio').css('display', 'block');
+    // })
+    // $('#nav-resume').on('click', () => {
+    //     $('#resume').css('display', 'block');
+    // })
+    // $('#nav-projects').on('click', () => {
+    //     $('#projects').css('display', 'block');
+    // })
+    
+    $('#nav-home').on('click', () => {
+        const $contentItems = $('.content-item');
+        console.log($contentItems)
+
+        for (let i = 0; i < $contentItems.length; i++) {
+            const objectClassesCollection = $contentItems[i].classList;
+            objectClassesCollection.remove("show-content");
+            // prints each content classList
+            console.log(objectClassesCollection);
+        }
+
+        $('#home').addClass('show-content');
+    })
+
+    $('#nav-bio').on('click', () => {
+        const $contentItems = $('.content-item');
+        console.log($contentItems)
+
+        for (let i = 0; i < $contentItems.length; i++) {
+            const objectClassesCollection = $contentItems[i].classList;
+            objectClassesCollection.remove("show-content");
+            // prints each content classList
+            console.log(objectClassesCollection);
+        }
+
+        $('#bio').addClass('show-content');
+    })
+
+    $('#nav-resume').on('click', () => {
+        const $contentItems = $('.content-item');
+        console.log($contentItems)
+
+        for (let i = 0; i < $contentItems.length; i++) {
+            const objectClassesCollection = $contentItems[i].classList;
+            objectClassesCollection.remove("show-content");
+            // prints each content classList
+            console.log(objectClassesCollection);
+        }
+
+        $('#resume').addClass('show-content');
+    })
+
+    $('#nav-projects').on('click', () => {
+        const $contentItems = $('.content-item');
+        console.log($contentItems)
+
+        for (let i = 0; i < $contentItems.length; i++) {
+            const objectClassesCollection = $contentItems[i].classList;
+            objectClassesCollection.remove("show-content");
+            // prints each content classList
+            console.log(objectClassesCollection);
+        }
+
+        $('#projects').addClass('show-content');
+    })
 })
